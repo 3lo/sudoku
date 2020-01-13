0d84e769ce3684ced37d793c88c72bbc6e9b0a48
Eric <41165769+3lo@users.noreply.github.com>
2020-01-13 09:52:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ print("sudoku")
 
 cv.namedWindow('image', cv.WINDOW_NORMAL)  # creates GUI window named Image.
 cv.waitKey(0)  # waits for any keyboard event. 0 means indefinitely
-# key f = 102
-# cv.destroyAllWindows()  # destroys the window we created
 
-# Goal: Snippet function to capture sudoku board
-# Then solve sudoku board
-# cv.imshow() image of sudoku board with program input answers into appropriate boxes
 
-# sudoku solution has to deal with backtracking ?
+def displayIMG():
+    # cv.imshow() image of sudoku board with program input answers into appropriate blank boxes
+    pass
+
+
+def solve():
+    # solve sudoku board
+    # solution can be find with backtracking - recursion
+    displayIMG()
+    pass
+
+
+if cv.waitKey(0) == ord('f'):
+    # Screen capture sudoku.com board
+    # Crop image to only the board
+    solve()
+    print('f')
+
+cv.destroyAllWindows()  # destroys the window we created
