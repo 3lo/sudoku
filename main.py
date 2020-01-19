a088272629cba6a30ee6261ffcff06747717074a
Eric <41165769+3lo@users.noreply.github.com>
2020-01-19 18:59:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,20 +20,6 @@ def solve():
     pass
 
 
-grid = [[0, 7, 0, 0, 0, 4, 1, 3, 0],
-        [9, 4, 5, 6, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 5, 7, 0, 0, 6],
-        [0, 5, 0, 0, 3, 0, 0, 0, 4],
-        [6, 9, 3, 0, 4, 0, 0, 0, 2],
-        [4, 8, 2, 1, 0, 6, 0, 5, 7],
-        [5, 0, 0, 3, 7, 0, 0, 6, 0],
-        [0, 6, 0, 0, 0, 0, 0, 8, 1],
-        [8, 0, 9, 5, 0, 0, 0, 7, 0]]
-
-for i in range(9):  # printing grid
-    print(grid[i])
-
-
 def grabImg():
     img = ImageGrab.grab(bbox=(362, 240, 862, 740))  # captures screen - bbox captures portion [x, y, width, height]
     img_np = np.array(img)  # converting img to array format so cv can read
