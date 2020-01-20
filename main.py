8e167fe246d035854833a1a87e4536029b169e25
Eric <41165769+3lo@users.noreply.github.com>
2020-01-19 22:06:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 cv.namedWindow('image', cv.WINDOW_NORMAL)  # creates GUI window named Image.
 cv.waitKey(0)  # waits for any keyboard event. 0 means indefinitely
 
+grid = [[0, 7, 0, 0, 0, 4, 1, 3, 0],
+        [9, 4, 5, 6, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 5, 7, 0, 0, 6],
+        [0, 5, 0, 0, 3, 0, 0, 0, 4],
+        [6, 9, 3, 0, 4, 0, 0, 0, 2],
+        [4, 8, 2, 1, 0, 6, 0, 5, 7],
+        [5, 0, 0, 3, 7, 0, 0, 6, 0],
+        [0, 6, 0, 0, 0, 0, 0, 8, 1],
+        [8, 0, 9, 5, 0, 0, 0, 7, 0]]
 
-def displayIMG():
-    # cv.imshow()  # image of sudoku board with program input answers into appropriate blank boxes
-    pass
 
-
-def solve():
-    # solve sudoku board - can be found with backtracking - recursion
-    # List of path - and List of numbers we visited but left to get rid of infinite loops
-    displayIMG()
-    pass
+def solve(board):
+    for i in range(len(board)):  # row
+        for j in range(len(board[0])):  # column
+            if board[i][j] == 0:
+                # code to change 0 into number
+                print(i, j)
 
 
 def grabImg():
@@ -32,6 +38,6 @@ def grabImg():
 
 if cv.waitKey(0) == ord('f'):
     grabImg()  # function screen captures sudoku.com and is cropped in on the sudoku board
-    solve()
+    solve(grid)
 
 cv.destroyAllWindows()  # destroys the window we created
